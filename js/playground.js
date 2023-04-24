90cd4daf0a9d13565de1209cfefb73d58aaa694a
Guchalo <a22202617@alunos.ulht.pt>
2023-04-24 16:06:16 +0100

    darkmode a funcionar

--- js/playground.js
+++ js/playground.js
@@ -1,3 +1,17 @@
 function mostrar(e) {
-    alert(`ahhhhh`);
-   }
+     
+  var btn = document.querySelector("#btn");
+  var cor = document.getElementById("content").style;
+  var title = document.getElementById("title").style;
+  if (btn.className == "fa-solid fa-sun fa-fade fa-lg") {
+    btn.className = "fa-solid fa-moon fa-fade fa-xl";
+    title.color = "#000000";
+    btn.style.color = "#000000";
+    cor.backgroundColor = "#ffffffb9";
+  } else {
+    btn.className = "fa-solid fa-sun fa-fade fa-lg";
+    title.color = "#ffffff";
+    btn.style.color = "#ffffff";
+    cor.backgroundColor = "#000000b9";
+  }
+}
